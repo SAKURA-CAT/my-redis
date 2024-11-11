1b7454744afef347c6f8ecfba2d3e3a4c6f9d87c
KAAANG <79990647+SAKURA-CAT@users.noreply.github.com>
2024-11-11 21:37:44 +0800

    feat: pong response

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
 #![allow(unused_imports)]
+
+use std::io::Write;
 use std::net::TcpListener;
 
 fn main() {
@@ -8,8 +10,8 @@ fn main() {
     let listener = TcpListener::bind("127.0.0.1:6379").unwrap();
     for stream in listener.incoming(){
         match stream {
-            Ok(_stream) => {
-                println!("accepted new connection");
+            Ok(mut _stream) => {
+                _stream.write_all(b"+PONG\r\n").unwrap()
             }
             Err(e) => {
                 println!("error: {}", e);
