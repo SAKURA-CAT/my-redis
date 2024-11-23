7f445c804e06f919e9849f4d160b2248fd9318e9
KAAANG <79990647+SAKURA-CAT@users.noreply.github.com>
2024-11-23 21:27:32 +0800

    feat: echo and pong

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,6 @@
-
 mod resp;
 
-use std::io::{Read, Write};
+use crate::resp::Value;
 use tokio::net::{TcpListener, TcpStream};
 
 #[tokio::main]
@@ -13,24 +12,53 @@ async fn main() {
     loop {
         let stream = listener.accept().await;
         match stream {
-            Ok((stream, _)) =>{
+            Ok((stream, _)) => {
                 println!("accepted new connection");
-                tokio::spawn(async move {
-                    handle_conn(stream).await
-                });
+                tokio::spawn(async move { handle_conn(stream).await });
             }
             Err(e) => {
                 println!("error: {}", e);
-                }
+            }
         }
     }
 }
 
-
-async fn handle_conn(mut stream: TcpStream) {
+// *2\r\n$4\r\nECHO\r\n$3\r\nhey\r\n
+async fn handle_conn(stream: TcpStream) {
     // Every connection will be created a new RespParser
-    let parser = resp::RespParser::new(stream);
+    let mut parser = resp::RespParser::new(stream);
+    println!("Handle a conn");
     loop {
+        let value = parser.read().await.unwrap();
+        println!("read value: {:?}", value);
+        let response = if let Some(v) = value {
+            let (command, args) = extract_command(v).await.unwrap();
+            match command.as_str() {
+                "ping" => Value::SimpleString("PONG".to_string()),
+                "echo" => args.first().unwrap().clone(),
+                c => panic!("unrecognized command: {}", c),
+            }
+        } else {
+            break;
+        };
+        println!("response: {:?}", response);
+        parser.write(response).await.unwrap();
+    }
+}
 
+async fn extract_command(value: Value) -> Result<(String, Vec<Value>), anyhow::Error> {
+    match value {
+        Value::Array(a) => Ok((
+            unpack_bulk_str(a.first().unwrap().clone())?,
+            a.into_iter().skip(1).collect(),
+        )),
+        _ => Err(anyhow::anyhow!("expecting  command format")),
+    }
+}
+
+fn unpack_bulk_str(value: Value) -> Result<String, anyhow::Error> {
+    match value {
+        Value::BulkString(s) => Ok(s),
+        _ => Err(anyhow::anyhow!("Expected command to be a bulk string")),
     }
 }
